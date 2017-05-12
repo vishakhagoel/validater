2f7da7e46ea74e490eeb0483f1c94f9ec17a0f67
Vish <vigoel@ucsc.edu>
2017-05-12 15:49:44 -0700

    Bug fix for no owners file and beautification

--- approval_validator.py
+++ approval_validator.py
@@ -6,8 +6,9 @@ from pathlib import Path
 from collections import defaultdict
 
 
-# global variables
-ROOT = '/Users/vishakhagoel/Desktop/test/twitter/repo_root/'
+# Constants
+CURRENT_WORKING_DIRECTORY = os.getcwd()
+ROOT = CURRENT_WORKING_DIRECTORY + '/repo_root/'
 OWNERS = '/OWNERS'
 DEPENDENCIES = '/DEPENDENCIES'
 APPROVED = 'Approved'
@@ -22,34 +23,40 @@ def read_file(file_path):
 #   Creates a map of all dependencies for the given repository
 #   { key : [value] } where "value" is a list of directories dependent on "key"
 #   That is, for each directory A, finds all the [directories] that are dependent on A
-def build_dependency_map(directory):
+def build_dependency_map(root_directory):
     dependency_map = defaultdict(list)
-    for root, dirs, files in os.walk(directory):
-        subdir = os.path.basename(root)         # subdirs : follow, message, tweet, or user
-        dependency_file = Path(directory + subdir + DEPENDENCIES)           # path of file
+    for root, dirs, files in os.walk(root_directory):
+        current_dir = os.path.basename(root)         # subdirs : follow, message, tweet, or user
+        dependency_file = Path(root_directory + current_dir + DEPENDENCIES)           # path of file
         dependency_data = read_file(dependency_file) if dependency_file.exists() else None
-        if dependency_data == None:
+        if dependency_data is None:
             continue
-        #   Add dependencies to the map
-        for index in range(len(dependency_data)):
-            dependency_map.setdefault(dependency_data[index], []).append(directory + subdir)
+        #add dependencies to the map
+        for dependency in range(len(dependency_data)):
+            dependency_map.setdefault(dependency_data[dependency], []).append(root_directory + current_dir)
     return dependency_map
 
 
 #   Checks if even one of the approvers is the owner of the directory
 #   If owners file doesn't exist, function checks it in the parent directory of the current directory
 def check_owners(approvers, directory_path):
+    #if there is no owner till the repo's root directory for a file,
+    #then assume user has the right to approve that file and return true
+    if directory_path == CURRENT_WORKING_DIRECTORY:
+        return True
     owners_file = Path(str(directory_path) + OWNERS)
-    owners_data = read_file(owners_file) if owners_file.exists() else None
+    owners = read_file(owners_file) if owners_file.exists() else None
     result = False
-    #if owners file in this directory doesn't exist check parent
-    if owners_data == None:
-        result = check_owners(approvers, os.path.dirname(str(directory_path)))
-    else:
-        for index in range(len(owners_data)):
-            if owners_data[index] in approvers:
+    #if any of the approvers is an owner then return true
+    if owners:
+        for owner in range(len(owners)):
+            if owners[owner] in approvers:
                 return True
-    return False or result
+        return False
+    #if owners file doesn't exist in this directory, check parent
+    else:
+        result = check_owners(approvers, os.path.dirname(str(directory_path)))
+    return result
 
 
 #   If any directory is dependent on the given directory, check approvers for the dependent dirs
@@ -59,16 +66,17 @@ def check_dependencies(approvers, directory_path):
     dependency_list = []
     if directory_path in dependency_map:
         dependency_list = dependency_map[directory_path]
-        for index in range(len(dependency_list)):
-            if validate_approval(approvers, dependency_list[index]) == False:
+        for dependency in range(len(dependency_list)):
+            if validate_approval(approvers, dependency_list[dependency]) == False:
                 return False
+    return True
 
 
 #   Returns Approved if sufficient approvals were provided
 #   Returns Not Approved otherwise
 def validate_approval(approvers, files_changed):
-    for index in range(len(files_changed)):
-        directory_path = Path(ROOT + os.path.dirname(files_changed[index]))
+    for file in range(len(files_changed)):
+        directory_path = Path(ROOT + os.path.dirname(files_changed[file]))
         if check_owners(approvers, directory_path) == False:
             return NOT_APPROVED
         if check_dependencies(approvers, directory_path) == False:
